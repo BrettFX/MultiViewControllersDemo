b2ce8bc6666f4325295368241ac6e81427dc3e96
Brett Allen <brettallen@Bretts-MacBook-Pro.local>
2017-08-18 12:34:49 -0400

    Re-added second view controller

--- MultiViewControllersDemo/MultiViewControllersDemo/SecondViewController.swift
+++ MultiViewControllersDemo/MultiViewControllersDemo/SecondViewController.swift
@@ -21,7 +21,7 @@ class SecondViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var myLabel: UILabel!
     
     @IBOutlet weak var myTextField: UITextField!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -41,7 +41,7 @@ class SecondViewController: UIViewController, UITextFieldDelegate {
         
         print("The value of myStringValue is: \(myStringValue!)")
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -59,15 +59,15 @@ class SecondViewController: UIViewController, UITextFieldDelegate {
     }
     
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
