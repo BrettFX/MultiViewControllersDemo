2fe29f77b3f108b144762ac3d88b487f78f07c7c
Brett Allen <brettallen777@gmail.com>
2017-08-18 12:19:25 -0400

    Merged previous files over

--- MultiViewControllersDemo/MultiViewControllersDemo/ViewController.swift
+++ MultiViewControllersDemo/MultiViewControllersDemo/ViewController.swift
@@ -8,11 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, MyProtocol, UITextFieldDelegate {
+    
+    @IBOutlet weak var myTextField: UITextField!
+    
+    @IBOutlet weak var myLabel: UILabel!
+    
+    var valueSentFromSecondViewController: String? = ""
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        super.viewDidLoad()        
+        self.myTextField.delegate = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let valueToDisplay: String = valueSentFromSecondViewController! == "" ? "None" : valueSentFromSecondViewController!
+        
+        print("Value from display = \(valueToDisplay)")
+        
+        if(valueSentFromSecondViewController != ""){
+            myLabel.text! = "Welcome \(valueSentFromSecondViewController ?? ""), you are quite the programming genious!"
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool{
+        self.view.endEditing(true)
+        return false
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +43,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func myButtonTapped(_ sender: Any) {
+        if(myTextField.text?.isEmpty)!{
+            return
+        }
+        
+        let secondViewController = self.storyboard?.instantiateViewController(withIdentifier: "SecondViewController") as! SecondViewController
+        
+        secondViewController.myStringValue = myTextField.text
+        
+        //IMPORTANT: for being able to pass data back
+        secondViewController.delegate = self
+        
+        self.navigationController?.pushViewController(secondViewController, animated: true)
+    }
+    
+    func setResultOfBusinessLogic(valueSent: String){
+        self.valueSentFromSecondViewController = valueSent
+    }
 }
 
