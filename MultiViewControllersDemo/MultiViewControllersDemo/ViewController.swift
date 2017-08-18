1e2a5293fdbef1e853c18f81dfa2547b5bcdb3a7
Brett Allen <brettallen@Bretts-MacBook-Pro.local>
2017-08-18 01:05:52 -0400

    Created objects and outlets and embeded first view controller into navigation controller

--- MultiViewControllersDemo/MultiViewControllersDemo/ViewController.swift
+++ MultiViewControllersDemo/MultiViewControllersDemo/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var myTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func myButtonTapped(_ sender: Any) {
+        
+    }
 }
 
